32c71ebbea9d4ad7447d5d9d6d51caf3b4c07c9c
Shelby Morrow <shelbymorrow@Shelbys-MBP.attlocal.net>
2020-11-05 10:10:05 -0600

    Variable learning

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,12 @@
 public class helloworld {
     public static void main(String[] args){
-        System.out.println("Hello World");
+        System.out.println("Hello Shelby");
+
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = 6; 
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        System.out.println(myTotal);
+        System.out.println(myFirstNumber);
     }
 }
