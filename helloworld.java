318d2c47011ec6148071a3ae5b11a39c99bf33b8
Shelby Morrow <shelbymorrow@Shelbys-MBP.attlocal.net>
2020-11-06 13:25:20 -0600

    Simple arthimatic

--- helloworld.java
+++ helloworld.java
@@ -4,9 +4,11 @@ public class helloworld {
 
         int myFirstNumber = (10 + 5) + (2 * 10);
         int mySecondNumber = 12;
-        int myThirdNumber = 6; 
+        int myThirdNumber = myFirstNumber * 2; 
         int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        int myLastOne = 1000 - myTotal;
         System.out.println(myTotal);
         System.out.println(myFirstNumber);
+        System.out.println(myLastOne);
     }
 }
