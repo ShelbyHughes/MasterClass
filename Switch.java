316fd58f0c2faa4d63031479494317f8ea46ef90
Shelby Morrow <shelbymorrow@Shelbys-MBP.attlocal.net>
2020-11-12 09:00:28 -0600

    switch statements complete

--- Switch.java
+++ Switch.java
@@ -26,6 +26,62 @@ public class Switch {
                 break;
         }
 
-        // more code here
+        // Create a new switch statement using char instead of int
+        // create a new char variable
+        // create a switch statement testing for
+        // A, B, C, D, or E
+        // display a message if any of these are found and then break 
+        // Add a default which displays a message saying not found
+
+        char character = 'A';
+        switch(character){
+            case 'A':
+                System.out.println("This is A");
+                break;
+            case 'B':
+                System.out.println("This is B");
+                break;
+            case 'C':
+                System.out.println("This is C");
+                break;
+            case 'D':
+                System.out.println("This is D");
+                break;
+            case 'E':
+                System.out.println("This is E");
+                break;
+            default:
+                System.out.println("Not Found");
+                break;
+        }
+
+        switch(character){
+            case 'A':
+                System.out.println("A was found");
+                break;
+            case 'B':
+                System.out.println("B was found");
+                break;
+            case 'C': case 'D': case 'E':
+                System.out.println(character + " was found");
+                break;
+            default:
+                System.out.println("Could not find A, B, C, D, or E");
+                break; 
+        }
+
+        String month = "JANUARy";
+        switch(month.toLowerCase()){
+            case "january":
+                System.out.println("Jan");
+                break;
+            case "june":
+                System.out.println("Jun");
+                break;
+            default:
+                System.out.println("Not sure");
+                break;
+        }
+
     }
 }
