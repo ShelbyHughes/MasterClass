58eec7350e0057d99c62518cd3f7895dae51b0e9
Shelby Morrow <shelbymorrow@Shelbys-MBP.attlocal.net>
2020-11-17 15:40:02 -0600

    resolves #23

---
+++ CodingChallenges/SampleCodingChallenge/SharedDigit.java
@@ -0,0 +1,20 @@
+package CodingChallenges.SampleCodingChallenge;
+
+public class SharedDigit {
+    public static void main(String[] args){
+        System.out.println(hasSharedDigit(12, 23));
+        System.out.println(hasSharedDigit(9, 99));
+        System.out.println(hasSharedDigit(15, 55));
+    }
+    public static boolean hasSharedDigit(int p, int q){
+        if(p < 10 || p > 99 || q < 10 || q > 99){
+            return false;
+        }
+        int x = p % 10;
+        int y = q % 10;
+        p /= 10;
+        q /= 10;
+        return(p == q|| p == y || x == q || x == y);
+
+    }
+}
