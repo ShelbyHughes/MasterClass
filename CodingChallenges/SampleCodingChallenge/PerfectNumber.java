b1f224c7358d33d4f47d2e55e372b89407ed2ee7
Shelby Morrow <shelbymorrow@Shelbys-MBP.attlocal.net>
2020-11-19 09:03:48 -0600

    resolve #27

---
+++ CodingChallenges/SampleCodingChallenge/PerfectNumber.java
@@ -0,0 +1,23 @@
+package CodingChallenges.SampleCodingChallenge;
+
+public class PerfectNumber {
+    public static void main(String[] args){
+        System.out.println(isPerfectNumber(6));
+        System.out.println(isPerfectNumber(28));
+        System.out.println(isPerfectNumber(5));
+        System.out.println(isPerfectNumber(-1));
+    }
+    public static boolean isPerfectNumber(int number){
+        int sum = 0;
+        if(number < 1){
+            return false;
+        }else{
+            for(int i = 1; i < number; i++){
+                if(number % i == 0){
+                    sum += i;    
+                }
+            }
+        }
+        return (sum == number);
+    }
+}
