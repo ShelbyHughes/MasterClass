2f858b5abf7494bcd200e2c333be863df0e9e6cd
Shelby Morrow <shelbymorrow@Shelbys-MBP.attlocal.net>
2020-12-02 14:57:43 -0600

    resolves #33

--- CodingChallenges/SampleCodingChallenge/InputCalculator.java
+++ CodingChallenges/SampleCodingChallenge/InputCalculator.java
@@ -6,5 +6,24 @@ public class InputCalculator {
     public static void main(String[] args){
         inputThenPrintSumAndAverage();
     }
-    
+    public static void inputThenPrintSumAndAverage(){
+        Scanner scanner = new Scanner(System.in);
+        
+        int sum = 0, count = 0;
+        while(true){
+            boolean isInt = scanner.hasNextInt();
+            if(isInt){
+                int num = scanner.nextInt();
+                sum += num;
+                count++;
+            }else{
+                break;
+            }
+            scanner.nextLine();
+        }
+
+        System.out.println("SUM = " + sum + " AVG = " + Math.round((double)sum/count));
+
+        scanner.close();
+    }
 }
