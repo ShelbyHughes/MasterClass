a809d007ca19a3629cdd1e3cb42df4ed065fe5df
Shelby Morrow <shelbymorrow@Shelbys-MBP.attlocal.net>
2020-12-02 14:32:02 -0600

    resolves #32

---
+++ CodingChallenges/SampleCodingChallenge/InputCalculator.java
@@ -0,0 +1,10 @@
+package CodingChallenges.SampleCodingChallenge;
+
+import java.util.Scanner;
+
+public class InputCalculator {
+    public static void main(String[] args){
+        inputThenPrintSumAndAverage();
+    }
+    
+}
