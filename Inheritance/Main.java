1e0741f4bfaf02f985309cb9148d0584476ca84f
Shelby Morrow <shelbymorrow@Shelbys-MBP.attlocal.net>
2020-12-09 13:16:59 -0600

    inheritance

---
+++ Inheritance/Main.java
@@ -0,0 +1,7 @@
+package Inheritance;
+
+public class Main {
+    Animal animal = new Animal("Animal", 1, 1, 5, 5);
+    Dog dog = new Dog("Yorkie", 8, 20, 2, 4, 1, 20, "long silky");
+    dog.eat();
+}
