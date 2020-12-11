9a80e535804e35dfd078db2ec8a4b14e10de4a2d
Shelby Morrow <shelbymorrow@Shelbys-MBP.attlocal.net>
2020-12-11 14:11:23 -0600

    inheritance challenge

--- InheritanceChallenge/Mustang.java
+++ InheritanceChallenge/Mustang.java
@@ -1,4 +1,10 @@
 package InheritanceChallenge;
 
-public class Mustang extends Car{   
+public class Mustang extends Car{ 
+    private int roadServiceMonths;
+    
+    public Mustang(int roadServiceMonths){
+        super("Mustang", "RWD", 4, 2, 5, true);
+        this.roadServiceMonths = roadServiceMonths;
+    }
  }
